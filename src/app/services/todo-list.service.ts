a87a85935fdde1dfaff3d6c07464f51c539bcdbd
uday9553 <56838410+uday9553@users.noreply.github.com>
2020-04-02 10:47:28 +0530

    delete with body implementation

--- src/app/services/todo-list.service.ts
+++ src/app/services/todo-list.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Todos } from '../list-todos/list-todos.component';
+import { Observable } from 'rxjs';
 //import { HttpClient,HttpClientModule } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
@@ -29,10 +30,30 @@ export class TodoListService {
     
     return this.http.post(`http://localhost:8080/createTodo/`,todo);
   }
+  
+   createTodoWithBody(todo:Todo){
+     let body=JSON.stringify(todo);
+    let options={
+      headers: new HttpHeaders({'Content-Type':'application/json'}),
+      body:body
+    };
+    return this.http.post(`http://localhost:8080/createTodoWithBody/`,options);
+  }
   createBasicAuthenticationHttpHeader(){
     let username='user';
     let password='pass';
     let basicAuthHeaderString='Basic'+window.btoa(username+':'+password);
     return basicAuthHeaderString;
   }
+  
+  deleteByBody(todoList:Todo[]):Observable<Todo[]>{
+    let body=JSON.stringify(todoList);
+    let options={
+      headers: new HttpHeaders({'Content-Type':'application/json'}),
+      body:body
+    };
+    return this.http.delete<Todo[]>(`url`,options);
+  }
+  
+  
 }
